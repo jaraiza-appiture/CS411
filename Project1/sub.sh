9289403996153fde008d75a13dbc00bcb12f56e3
Araiza <jovan.araiza@pleiades.tricity.wsu.edu>
2019-09-07 14:08:02 -0700

    adding shell script

---
+++ Project1/sub.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#To run this script, please use "sbatch -N<numberofnodes> <path>/sub.sh"
+
+# setting the max wallclock time after which the job will be killed; 
+# note: the format is hr:min:sec (default set here to 10 mins)
+SBATCH --time=00:10:00
+
+
+#MPI helloworld example - this line is a comment
+#"np" stands for number of processes.
+#this command will run the job on 8 processes. 
+mpirun -np 2 ./proj_1
+
+
+# you can add more lines of commands here, if you wish.
