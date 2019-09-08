edfe1cf0e4da704f29a697886e68f7a12689fbab
Jovan Araiza <jovan.araiza@wsu.edu>
2019-09-07 19:12:25 -0700

    s

--- Project1/sub.sh
+++ Project1/sub.sh
@@ -4,7 +4,7 @@
 
 # setting the max wallclock time after which the job will be killed; 
 # note: the format is hr:min:sec (default set here to 10 mins)
-SBATCH --time=00:10:00
+#SBATCH --time=00:10:00
 
 
 #MPI helloworld example - this line is a comment
@@ -24,8 +24,7 @@ SBATCH --time=00:10:00
 #     mpirun -np 2 ./proj_1_blocking $i >> out$i.txt;
 #     ((i=i*2))
 # done
-END = 5
-for ((i=1;i<=END;i++)); do
+for ((i=1;i<=5;i++)); do
     echo $i
 done
 # you can add more lines of commands here, if you wish.
