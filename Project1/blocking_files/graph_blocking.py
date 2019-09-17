07c10dd995d8b60589d843ecdaa1d5e4827ee1bf
Jovan Araiza <jovan.araiza@wsu.edu>
2019-09-16 17:34:03 -0700

    results ready for report

--- Project1/blocking_files/graph_blocking.py
+++ Project1/blocking_files/graph_blocking.py
@@ -1,10 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 DATA_PATH = './outAll.txt'
 NET_DATA = {0:{}, 1:{}} # 0 = recv, 1 = send
 RECV = 0
 SEND = 1
+OUT_F = open('results.txt', 'w')
 
 def load_data():
     """
@@ -33,11 +35,32 @@ def recv_graph():
         x_msg_size.append(msg_size)
         y_time_avg.append(np.average(times))
         y_time_stddev.append(np.std(times))
-    plt.title('Blocking Recv Times vs. Message Size')
+    
+    plt.figure(figsize=(8,6), dpi=100, facecolor='w', edgecolor='k')
+    plt.title('Message Size vs. Blocking Recv Times')
     plt.xlabel('message size (bytes)')
     plt.ylabel('recv time (microseconds)')
-    plt.errorbar(x_msg_size, y_time_avg, yerr=y_time_stddev, fmt='-o')
-    plt.show()
+    plt.errorbar(x_msg_size, y_time_avg, yerr=y_time_stddev, fmt='o-b', ecolor='r')
+    plt.savefig('recv_plot.png')
+    
+    # split
+    pre_time_avg = []
+    pre_msg_size = []
+    post_time_avg = []
+    post_msg_size = []
+    
+    for i, size in enumerate(x_msg_size):
+        if size < 256:
+            pre_time_avg.append(y_time_avg[i])
+            pre_msg_size.append(size)
+        else:
+            post_time_avg.append(y_time_avg[i])
+            post_msg_size.append(size)
+    post_msg_size = np.array(post_msg_size).reshape(-1, 1)
+    post_time_avg = np.array(post_time_avg).reshape(-1, 1)
+    latency = np.average(pre_time_avg)
+    reg = LinearRegression().fit(post_msg_size, post_time_avg)
+    print('Recv Latency:', latency, 'Recv Linear Regression slope:', reg.coef_, file=OUT_F)
 
 def send_graph():
     """
@@ -51,13 +74,19 @@ def send_graph():
         x_msg_size.append(msg_size)
         y_time_avg.append(np.average(times))
         y_time_stddev.append(np.std(times))
-    plt.title('Blocking Send Times vs. Message Size')
+    
+    plt.figure(figsize=(8,6), dpi=100, facecolor='w', edgecolor='k')
+    plt.title('Message Size vs. Blocking Send Times')
     plt.xlabel('message size (bytes)')
     plt.ylabel('send time (microseconds)')
-    plt.errorbar(x_msg_size, y_time_avg, yerr=y_time_stddev, fmt='-o')
-    plt.show()
+    plt.errorbar(x_msg_size, y_time_avg, yerr=y_time_stddev, fmt='o-b', ecolor='r')
+    plt.savefig('send_plt.png')
+    
+    latency = np.average(y_time_avg)
+    print('Send latency:', latency, file=OUT_F)
 
 if __name__ == '__main__':
     load_data()
     send_graph()
     recv_graph()
+    OUT_F.close()
