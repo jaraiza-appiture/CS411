00b106a4448ce2de3def7b6a5cedb0918aa55352
Jovan Araiza <jovan.araiza@wsu.edu>
2019-09-28 15:05:04 -0700

    scatter ready for testing

--- Project2/reduction.c
+++ Project2/reduction.c
@@ -1,38 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <mpi.h> 
 #include <assert.h>
 #include <sys/time.h>
 
-int generate_array(int size)
+int *GenerateArray(int size)
 {
-    //generate random int array of give size...
-    //use malloc and find good rand function lib
-    // or could have each process generate its own part if array too...
+    int *arr = malloc(sizeof(int)*size);
+
+    for(int i=0; i < size; i++)
+    {
+        arr[i] = rand();
+    }
+
+    return arr;
 }
 
-int operationSum(int array[])
+int operationSum(int array[], int size)
 {
-    int length = sizeof(array)/sizeof(int);
+
     int sum = 0;
-    for(int i = 0; i < length; i++)
+    for(int i = 0; i < size; i++)
     {
         sum = sum + array[i];
     }
     return sum;
 }
 
-int my_reduce(int rank, int procs, int array[])
+int MyReduce(int array[], int size, int rank, int procs)
 {
     int buddy = 0, sum_buddy = 0;
-    int sum = operationSum(array);
+    int sum = operationSum(array, size);
     int time_steps = (int)ceil(log2((double)procs)) -1;
     int t = 1, x = 0;
     MPI_Status status;
 
     for(int k = 0; k <= time_steps; k++)
     {
-        buddy = rank ^ t; //XOR flipping operator
+        buddy = rank ^ t; // XOR flipping operator
         t << 1; // double num
 
         if(buddy < rank)
@@ -44,7 +50,6 @@ int my_reduce(int rank, int procs, int array[])
         {
             MPI_Send(&sum, 1, MPI_INT, buddy, 0, MPI_COMM_WORLD);
         }
-        
     }
 
     if(rank == (p-1))
@@ -56,23 +61,40 @@ int my_reduce(int rank, int procs, int array[])
 int main(int argc,char *argv[])
 {
 
-   int rank, p;
-   struct timeval t1, t2;
-   int size = 0; 
-   
-   MPI_Init(&argc, &argv);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-   MPI_Comm_size(MPI_COMM_WORLD, &p);
+    int rank, p;
+    struct timeval t1, t2;
+    int *arr, size = 0, root = 0;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-   if(argc >= 1)
-   {
-	size = atoi(argv[1]);
-   }
-   assert(p >= 2);
+    if(argc >= 1)
+    {
+        size = atoi(argv[1]);
+    }
+    assert(p >= 2);
+    
+    time_t t;
+    srand((unsigned)time(&t));
+    
+    
+    if(rank == root) // make array of given size
+    {
+        arr = GenerateArray(size);
+    }
 
-    //Idea for sending array parts around, only one particular rank will send array parts
-    // to the rest of the procs, will keep its own part.
-    // then we begin.... this may not work..
+    int items_per_proc = size / p;
+    
+    int * sub_arr = malloc(sizeof(int) * items_per_proc);
 
-   MPI_Finalize();
+    MPI_Scatter(arr, items_per_proc, MPI_INT, sub_arr, items_per_proc, MPI_INT, root, MPI_COMM_WORLD);
+
+    // MPI_Bcast(arr, size, MPI_INT, root, MPI_COMM_WORLD); // broadcast to all procs
+
+    // MPI_Barrier(MPI_COMM_WORLD); // synchronize all procs before starting reduction
+    
+    MyReduce(sub_arr, items_per_proc, rank, p);
+
+    MPI_Finalize();
 }
