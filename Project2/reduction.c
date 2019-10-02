9c11f88f3da80e24212bd19f533506b294fc5f93
smemon <samra.memon@wsu.edu>
2019-10-01 22:37:19 -0700

    added

--- Project2/reduction.c
+++ Project2/reduction.c
@@ -71,7 +71,7 @@ int MyNaive(int array[], int size, int rank, int procs)
     if (rank == (procs-1) )
     {
         MPI_Recv(&sum_buddy,4,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-        // sum = sum + sum_buddy ;
+        sum = sum + sum_buddy ;
 
     }
     	
