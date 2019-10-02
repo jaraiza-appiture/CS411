c3dd03f422fa83d92ddcd1cdd92e713a36c6e2ec
smemon <samra.memon@wsu.edu>
2019-10-01 20:33:16 -0700

    added

--- Project2/reduction.c
+++ Project2/reduction.c
@@ -156,9 +156,10 @@ int main(int argc,char *argv[])
     MPI_Barrier(MPI_COMM_WORLD); // synchronize all procs before marking end time
     gettimeofday(&t2, NULL);
     int time_mpireduce = elapsedTime(t1, t2);
-    printf("%d\n", sum_my_reduce);
-    printf("%d\n", sum_my_naive);
-    printf("%d\n", sum_mpi_reduce);
+    printf("sum_my_reduce   %d\n", sum_my_reduce);
+    printf("sum_my_naive    %d\n", sum_my_naive);
+    printf("sum_mpi_reduce  %d\n\n", sum_mpi_reduce);
+    
 
     if(rank == p-1)
     {
