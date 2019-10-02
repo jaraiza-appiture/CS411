17d9745326dcf41bec4a07f5c2262877d301da93
smemon <samra.memon@wsu.edu>
2019-10-01 18:58:25 -0700

    added

--- Project2/run_tests.sh
+++ Project2/run_tests.sh
@@ -12,12 +12,15 @@ SBATCH --time=00:10:00
 #this command will run the job on 8 processes.
 mpicc -o reduction reduction.c -w -lm
 rm results.csv
-n=1
-for i in 1 2 4 8 16 32 64
+n=1000
+while [ $n -le 1000000 ]
 do
-    while [ $n -le 100 ]
-    do
-        mpirun -np $i ./reduction $n >> results.csv        
-        n=$(( n*2 ))     # increments $n
-    done
+    mpirun -np 1 ./reduction $n >> results.csv 
+    mpirun -np 2 ./reduction $n >> results.csv        
+    mpirun -np 4 ./reduction $n >> results.csv        
+    mpirun -np 8 ./reduction $n >> results.csv        
+    mpirun -np 16 ./reduction $n >> results.csv        
+    mpirun -np 32 ./reduction $n >> results.csv        
+    mpirun -np 64 ./reduction $n >> results.csv        
+    n=$(( n*2 ))     # increments $n
 done
