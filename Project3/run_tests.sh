63d47275e88c49e3e7545ee84a91794cae8061af
Jovan Araiza <jovan.araiza@wsu.edu>
2019-10-29 02:46:16 -0700

    s

--- Project3/run_tests.sh
+++ Project3/run_tests.sh
@@ -12,12 +12,10 @@ SBATCH --time=00:10:00
 #this command will run the job on 8 processes.
 mpicc -o rand_gen rand_gen.c -w -lm
 rm results.csv
-n=1000                    #A  B  P          seed
-mpirun -np 1 ./rand_gen 10 5 9 9967 42 >> results.csv        
+n=16                    #A  B  P          seed
 
-# while [ $n -le 2048000 ]    
-# do
-#     mpirun -np 1 ./reduction $n >> results.csv
-    
-#     n=$(( n*2 ))     # increments $n
-# done
+while [ $n -le 1048000 ]    
+do
+    mpirun -np 1 ./rand_gen $n 5 9 9967 42 >> results.csv
+    n=$(( n*2 ))     # increments $n
+done
