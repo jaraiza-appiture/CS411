e8226ceedc0cd7fd5024b89c05743da7a6b5ae10
Jovan Araiza <jovan.araiza@wsu.edu>
2019-10-28 22:03:48 -0700

    adding shell script

---
+++ Project3/run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#To run this script, please use "sbatch -N<numberofnodes> <path>/sub.sh"
+
+# setting the max wallclock time after which the job will be killed; 
+# note: the format is hr:min:sec (default set here to 10 mins)
+SBATCH --time=00:10:00
+
+
+#MPI helloworld example - this line is a comment
+#"np" stands for number of processes.
+#this command will run the job on 8 processes.
+mpicc -o rand_gen rand_gen.c -w -lm
+rm results.csv
+n=1000
+mpirun -np 1 ./rand_gen 10 42 37 9999999967 42 >> results.csv        
+
+# while [ $n -le 2048000 ]    
+# do
+#     mpirun -np 1 ./reduction $n >> results.csv
+    
+#     n=$(( n*2 ))     # increments $n
+# done
