6eed48ff321d238f8dcbefb96fc34748e9102110
Jovan Araiza <jovan.araiza@wsu.edu>
2019-10-29 17:18:56 -0700

    graphing done

--- Project3/make_graphs.py
+++ Project3/make_graphs.py
@@ -1,19 +1,24 @@
 from matplotlib import pyplot as plt
 import numpy as np
 RESULTS = './results.csv'
-ALL_REDUCE = 0
-NAIVE_REDUCE = 1
-MPI_REDUCE = 2
+SERIAL_BASELINE = 0
+SERIAL_MATRIX = 1
+PARALLEL_PREFIX = 2
+
 def load_data(filename):
     in_file = open(filename, 'r')
     data = {}
     for line in in_file:
-        myreduce_time, mynaive_time, mpireduce_time, procs, elements, op_output = line.split(',')
+        #csv format: time_serial_baseline, time_serial_matrix, time_parallel_prefix, procs, elements, a, b, p, seed
+        time_serial_baseline, time_serial_matrix, time_parallel_prefix, procs, elements, a, b, p, seed = line.split(',')
         if not data.get(int(elements), False):
             data[int(elements)] = {}
         if not data[int(elements)].get(int(procs), False):
             data[int(elements)][int(procs)] = []
-        data[int(elements)][int(procs)].append((int(myreduce_time), int(mynaive_time), int(mpireduce_time), int(op_output)))
+        data[int(elements)][int(procs)].append((int(time_serial_baseline),
+                                                int(time_serial_matrix),
+                                                int(time_parallel_prefix),
+                                                int(a), int(b), int(p), int(seed)))
     in_file.close()
     
     return data
@@ -28,7 +33,7 @@ def make_graphs(data):
     
     x_axis_line = [1, 2, 4, 8, 16, 32, 64]
     
-    # ALL REDUCE
+    # SERIAL MATRIX
     lines_parallel_time = {}
     lines_speedup_time = {}
     lines_efficiency_time = {}
@@ -36,12 +41,11 @@ def make_graphs(data):
         lines_parallel_time[num_elements] = []
         lines_speedup_time[num_elements] = []
         lines_efficiency_time[num_elements] = []
-        avg_best_serial_time = np.average([time_tuple[NAIVE_REDUCE] for time_tuple in data[num_elements][1]]) # 1 proc
+        avg_best_serial_time = np.average([time_tuple[SERIAL_BASELINE] for time_tuple in data[num_elements][1]]) # 1 proc
         for num_procs, times in sorted(procs_dict.items()):
-            avg_allreduce_time = np.average([time_tuple[ALL_REDUCE] for time_tuple in times])
-            #avg_mpireduce_time = np.average([time_tuple[MPI_REDUCE] for time_tuple in times])
-            lines_parallel_time[num_elements].append(avg_allreduce_time)
-            speedup_score = speed_up(avg_best_serial_time, avg_allreduce_time)
+            avg_serial_matrix_time = np.average([time_tuple[SERIAL_MATRIX] for time_tuple in times])
+            lines_parallel_time[num_elements].append(avg_serial_matrix_time)
+            speedup_score = speed_up(avg_best_serial_time, avg_serial_matrix_time)
             lines_speedup_time[num_elements].append(speedup_score)
             efficiency_score = efficiency(speedup_score, num_procs)
             lines_efficiency_time[num_elements].append(efficiency_score)
@@ -51,11 +55,11 @@ def make_graphs(data):
     for num_elements, line in lines_parallel_time.items():
         plt.plot(x_axis_line, line, label="n = %d"%(num_elements))
     plt.legend(loc='upper right')
-    plt.title("All Reduce Parallel Time", fontsize=16, fontweight='bold')
+    plt.title("Serial Matrix Time", fontsize=16, fontweight='bold')
     plt.xlabel("Number of Processes")
     plt.ylabel("Parallel Time (microseconds)")
     plt.xticks(x_axis_line)
-    plt.savefig('./all_reduce_parallel_time.png')
+    plt.savefig('./serial_matrix_time.png')
     plt.show()
     
     # plot speed-up graph
@@ -63,11 +67,11 @@ def make_graphs(data):
     for num_elements, line in lines_speedup_time.items():
         plt.plot(x_axis_line, line, label="n = %d"%(num_elements))
     plt.legend(loc='upper right')
-    plt.title("All Reduce Speed Up", fontsize=16, fontweight='bold')
+    plt.title("Serial Matrix Speed Up", fontsize=16, fontweight='bold')
     plt.xlabel("Number of Processes")
     plt.ylabel("Speed Up Factor")
     plt.xticks(x_axis_line)
-    plt.savefig('./all_reduce_speed_up.png')
+    plt.savefig('./serial_matrix_speed_up.png')
     plt.show()
     
     # plot speed-up graph
@@ -75,14 +79,14 @@ def make_graphs(data):
     for num_elements, line in lines_efficiency_time.items():
         plt.plot(x_axis_line, line, label="n = %d"%(num_elements))
     plt.legend(loc='upper right')
-    plt.title("All Reduce Efficiency", fontsize=16, fontweight='bold')
+    plt.title("Serial Matrix Efficiency", fontsize=16, fontweight='bold')
     plt.xlabel("Number of Processes")
     plt.ylabel("Efficiency (%)")
     plt.xticks(x_axis_line)
-    plt.savefig('./all_reduce_efficiency.png')
+    plt.savefig('./serial_matrix_efficiency.png')
     plt.show()
     
-    # MPI REDUCE
+    # PARALLEL PREFIX
     lines_parallel_time = {}
     lines_speedup_time = {}
     lines_efficiency_time = {}
@@ -90,12 +94,11 @@ def make_graphs(data):
         lines_parallel_time[num_elements] = []
         lines_speedup_time[num_elements] = []
         lines_efficiency_time[num_elements] = []
-        avg_best_serial_time = np.average([time_tuple[NAIVE_REDUCE] for time_tuple in data[num_elements][1]]) # 1 proc
+        avg_best_serial_time = np.average([time_tuple[SERIAL_BASELINE] for time_tuple in data[num_elements][1]]) # 1 proc
         for num_procs, times in sorted(procs_dict.items()):
-            #avg_allreduce_time = np.average([time_tuple[ALL_REDUCE] for time_tuple in times])
-            avg_mpireduce_time = np.average([time_tuple[MPI_REDUCE] for time_tuple in times])
-            lines_parallel_time[num_elements].append(avg_mpireduce_time)
-            speedup_score = speed_up(avg_best_serial_time, avg_mpireduce_time)
+            avg_parallel_prefix_time = np.average([time_tuple[PARALLEL_PREFIX] for time_tuple in times])
+            lines_parallel_time[num_elements].append(avg_parallel_prefix_time)
+            speedup_score = speed_up(avg_best_serial_time, avg_parallel_prefix_time)
             lines_speedup_time[num_elements].append(speedup_score)
             efficiency_score = efficiency(speedup_score, num_procs)
             lines_efficiency_time[num_elements].append(efficiency_score)
@@ -105,11 +108,11 @@ def make_graphs(data):
     for num_elements, line in lines_parallel_time.items():
         plt.plot(x_axis_line, line, label="n = %d"%(num_elements))
     plt.legend(loc='upper right')
-    plt.title("MPI All Reduce Parallel Time", fontsize=16, fontweight='bold')
+    plt.title("Parallel Prefix Time", fontsize=16, fontweight='bold')
     plt.xlabel("Number of Processes")
     plt.ylabel("Parallel Time (microseconds)")
     plt.xticks(x_axis_line)
-    plt.savefig('./MPI_all_reduce_parallel_time.png')
+    plt.savefig('./parallel_prefix_time.png')
     plt.show()
     
     # plot speed-up graph
@@ -117,11 +120,11 @@ def make_graphs(data):
     for num_elements, line in lines_speedup_time.items():
         plt.plot(x_axis_line, line, label="n = %d"%(num_elements))
     plt.legend(loc='upper right')
-    plt.title("MPI All Reduce Speed Up", fontsize=16, fontweight='bold')
+    plt.title("Parallel Prefix Speed Up", fontsize=16, fontweight='bold')
     plt.xlabel("Number of Processes")
     plt.ylabel("Speed Up Factor")
     plt.xticks(x_axis_line)
-    plt.savefig('./MPI_all_reduce_speed_up.png')
+    plt.savefig('./parallel_prefix_speed_up.png')
     plt.show()
     
     # plot speed-up graph
@@ -129,14 +132,14 @@ def make_graphs(data):
     for num_elements, line in lines_efficiency_time.items():
         plt.plot(x_axis_line, line, label="n = %d"%(num_elements))
     plt.legend(loc='upper right')
-    plt.title("MPI All Reduce Efficiency", fontsize=16, fontweight='bold')
+    plt.title("Parallel Prefix Efficiency", fontsize=16, fontweight='bold')
     plt.xlabel("Number of Processes")
     plt.ylabel("Efficiency (%)")
     plt.xticks(x_axis_line)
-    plt.savefig('./MPI_all_reduce_efficiency.png')
+    plt.savefig('./parallel_prefix_efficiency.png')
     plt.show()
     
-    # NAIVE REDUCE
+    # SERIAL BASELINE
     lines_parallel_time = {}
     lines_speedup_time = {}
     lines_efficiency_time = {}
@@ -144,13 +147,11 @@ def make_graphs(data):
         lines_parallel_time[num_elements] = []
         lines_speedup_time[num_elements] = []
         lines_efficiency_time[num_elements] = []
-        avg_best_serial_time = np.average([time_tuple[NAIVE_REDUCE] for time_tuple in data[num_elements][1]]) # 1 proc
+        avg_best_serial_time = np.average([time_tuple[SERIAL_BASELINE] for time_tuple in data[num_elements][1]]) # 1 proc
         for num_procs, times in sorted(procs_dict.items()):
-            #avg_allreduce_time = np.average([time_tuple[ALL_REDUCE] for time_tuple in times])
-            #avg_mpireduce_time = np.average([time_tuple[MPI_REDUCE] for time_tuple in times])
-            avg_naivereduce_time = np.average([time_tuple[NAIVE_REDUCE] for time_tuple in times])
-            lines_parallel_time[num_elements].append(avg_naivereduce_time)
-            speedup_score = speed_up(avg_best_serial_time, avg_naivereduce_time)
+            avg_serial_baseline_time = np.average([time_tuple[SERIAL_BASELINE] for time_tuple in times])
+            lines_parallel_time[num_elements].append(avg_serial_baseline_time)
+            speedup_score = speed_up(avg_best_serial_time, avg_serial_baseline_time)
             lines_speedup_time[num_elements].append(speedup_score)
             efficiency_score = efficiency(speedup_score, num_procs)
             lines_efficiency_time[num_elements].append(efficiency_score)
@@ -160,11 +161,11 @@ def make_graphs(data):
     for num_elements, line in lines_parallel_time.items():
         plt.plot(x_axis_line, line, label="n = %d"%(num_elements))
     plt.legend(loc='upper right')
-    plt.title("Naive Reduce Parallel Time", fontsize=16, fontweight='bold')
+    plt.title("Serial Baseline Time", fontsize=16, fontweight='bold')
     plt.xlabel("Number of Processes")
     plt.ylabel("Parallel Time (microseconds)")
     plt.xticks(x_axis_line)
-    plt.savefig('./naive_reduce_parallel_time.png')
+    plt.savefig('./serial_baseline_time.png')
     plt.show()
     
     # plot speed-up graph
@@ -172,11 +173,11 @@ def make_graphs(data):
     for num_elements, line in lines_speedup_time.items():
         plt.plot(x_axis_line, line, label="n = %d"%(num_elements))
     plt.legend(loc='upper right')
-    plt.title("Naive Reduce Speed Up", fontsize=16, fontweight='bold')
+    plt.title("Serial Baseline Speed Up", fontsize=16, fontweight='bold')
     plt.xlabel("Number of Processes")
     plt.ylabel("Speed Up Factor")
     plt.xticks(x_axis_line)
-    plt.savefig('./naive_reduce_speed_up.png')
+    plt.savefig('./serial_baseline_speed_up.png')
     plt.show()
     
     # plot speed-up graph
@@ -184,11 +185,11 @@ def make_graphs(data):
     for num_elements, line in lines_efficiency_time.items():
         plt.plot(x_axis_line, line, label="n = %d"%(num_elements))
     plt.legend(loc='upper right')
-    plt.title("Naive Reduce Efficiency", fontsize=16, fontweight='bold')
+    plt.title("Serial Baseline Efficiency", fontsize=16, fontweight='bold')
     plt.xlabel("Number of Processes")
     plt.ylabel("Efficiency (%)")
     plt.xticks(x_axis_line)
-    plt.savefig('./naive_reduce_efficiency.png')
+    plt.savefig('./serial_baseline_efficiency.png')
     plt.show()
     
     
