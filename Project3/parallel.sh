446773086e2b009e303c7687a0517e66848b0bdf
Memon <samra.memon@pleiades.tricity.wsu.edu>
2019-11-07 13:14:38 -0800

    added

--- Project3/parallel.sh
+++ Project3/parallel.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
-#SBATCH --time=00:10:00
+SBATCH --time=00:10:00
 mpicc -o palgorithm palgorithm.c -lm
-                     //  A   B     Prime seed n 
+                     #    A   B     Prime seed n 
 mpirun -np 2 ./palgorithm 2069 3069 9973 10 8 >> ./a.out
 
-// n=$1
-// i=$n
-
-// while [ $i -le 1048576 ]
-// do
-//                         //  A   B     Prime seed n 
-//     mpirun -np $n ./parallel 127 2897 880681 1 $i >> ./.out
-//     i=$((i*2))
-// done
-    // A = atoi(argv[1]);
-    // B = atoi(argv[2]);
-    // Prime = atoi(argv[3]);
-    // seed = atoi(argv[4]);
-    // n = atoi(argv[5]);
-    //  A = 2069;
-    // B = 3069;
-    // Prime = 9973;
-    // seed = 10;
-    // n = 8;
+# n=$1
+# i=$n
+# while [ $i -le 1048576 ]
+# do
+#                         //  A   B     Prime seed n 
+#     mpirun -np $n ./parallel 127 2897 880681 1 $i >> ./.out
+#     i=$((i*2))
+# done
+    # A = atoi(argv[1]);
+    # B = atoi(argv[2]);
+    # Prime = atoi(argv[3]);
+    # seed = atoi(argv[4]);
+    # n = atoi(argv[5]);
+    #  A = 2069;
+    # B = 3069;
+    # Prime = 9973;
+    # seed = 10;
+    # n = 8;
