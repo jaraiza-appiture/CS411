23aff6fada335300e511ded786fb55825bff304e
smemon <samra.memon@wsu.edu>
2019-11-07 16:12:03 -0800

    phelper fixed

--- Project3/phelper.c
+++ Project3/phelper.c
@@ -215,3 +215,63 @@ Matrix ParallelPrefix(Matrix global, int procs, int rank, int Prime, int A, int 
     return local;
 }
 
+int * ParallelImplementation(int n, int A, int B, int Prime, int seed, int procs, int rank)
+{
+    Matrix *M_arr = (Matrix*)malloc(sizeof(Matrix) * (n/procs));
+    int  *ouch_arr = (int*)malloc(sizeof(int)*(n/procs));
+    int i;
+    //initialize all the xlocals to (A,0,B,1)
+    for(i =0; i < n/procs; i++) {
+        M_arr[i].M[0][0] = A;
+        M_arr[i].M[0][1] = 0;
+        M_arr[i].M[1][0] = B;
+        M_arr[i].M[1][1] = 1;
+    }
+    
+    Matrix global;  
+
+    //calculate M^n/p for all procs    
+   if (rank ==0) 
+	{
+	M_arr[0].M[0][0]=1;
+	M_arr[0].M[0][1]=0;
+	M_arr[0].M[1][0]=0;
+	M_arr[0].M[1][1]=1;
+	}
+
+    for(i =1; i < n/procs; i++) {
+       M_arr[i] =  multiplySquareMatMod( M_arr[i], M_arr[i-1], Prime);
+    }
+    //get the last irem in M_arr M^n/p
+    global =  M_arr[(n/procs)-1];
+
+    //pass the global to ParallelPrefix
+    Matrix offset;
+    offset = ParallelPrefix( global, procs, rank, Prime, A, B);
+   
+    ///now that we have offset
+    //we generate randomArr
+    int M_randArr[1][2], x0_1[1][2];
+   
+    // final ouput 
+    // [x0  1] => x0 = seed and 1 stays same
+    x0_1[0][0] = seed; x0_1[0][1] = 1;
+    int j;
+    
+    for ( i = 0; i < n / procs; i++) {
+        
+        //generate offset * M_arr[i]
+        M_arr[i] = multiplySquareMatMod(M_arr[i], offset, Prime);
+	    // printf("After getting multiplied by offset\n");
+	    // printMatrix(M_arr[i]);
+        //generate random numbers => M_randArr has everything in the end
+        multiplyRectMatMod(x0_1, M_arr[i], M_randArr, Prime);
+        ouch_arr[i]  = M_randArr[0][0];
+    }
+    
+
+
+ return ouch_arr;
+
+}
+
