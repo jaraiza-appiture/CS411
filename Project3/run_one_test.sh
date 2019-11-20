b58ff378eb18d80464490ef7388642d678b1a2da
Jovan Araiza <jovan.araiza@wsu.edu>
2019-11-20 12:55:58 -0800

    adding single sh file for demo

---
+++ Project3/run_one_test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#To run this script, please use "sbatch -N<numberofnodes> <path>/sub.sh"
+
+# setting the max wallclock time after which the job will be killed; 
+# note: the format is hr:min:sec (default set here to 10 mins)
+SBATCH --time=00:10:00
+
+
+#MPI helloworld example - this line is a comment
+#"np" stands for number of processes.
+#this command will run the job on 8 processes.
+mpicc -o rand_gen rand_gen.c -w -lm
+rm results.csv
+
+#          p             n A B P    seed
+mpirun -np 2 ./rand_gen $n 5 9 9967 42 >> results.csv
